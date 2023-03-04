b7475721080f86de4383df936ed8a0a1fbaa62bc
gfCrova <giancrova.cl5@gmail.com>
2023-03-04 13:40:37 -0300

    modificaciones en los ejercicios b y c

--- src/Clase3/Ejercicio1_C.java
+++ src/Clase3/Ejercicio1_C.java
@@ -1,2 +1,19 @@
-package Clase3;public class Ejercicio1_C {
+package Clase3;
+
+public class Ejercicio1_C {
+
+    public static void main(String[] args) {
+        main1(20);
+    }
+
+    public static void main1(int x) {
+        int[] vectorNumeros = {35, 9, 56, 105, 44, 2, 183, 92};
+        int suma = 0;
+        for (int i = 0; i < vectorNumeros.length; i++) {
+            if (vectorNumeros[i] > x) {
+               suma += vectorNumeros[i];
+            }
+        }
+        System.out.println(suma);
+    }
 }
