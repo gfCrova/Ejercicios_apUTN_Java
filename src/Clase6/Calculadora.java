f62f36ff1eb74884c0d50f1a2bd884f2f7ba5023
gfCrova <giancrova.cl5@gmail.com>
2023-03-17 20:39:35 -0300

    testeo de los metodos de la clase Calculadora

---
+++ src/Clase6/Calculadora.java
@@ -0,0 +1,23 @@
+package Clase6;
+
+public class Calculadora {
+
+    public Calculadora() {};
+
+    public double sumar(double unNumero, double otroNumero) {
+        return unNumero + otroNumero;
+    };
+
+    public double restar(double unNumero, double otroNumero) {
+        return unNumero - otroNumero;
+    };
+
+    public double multiplicar(double unNumero, double otroNumero) {
+        return unNumero * otroNumero;
+    };
+
+    public double dividir(double unNumero, double otroNumero) {
+        return unNumero / otroNumero;
+    };
+
+}
---
+++ src/Clase6/CalculadoraTest.java
@@ -0,0 +1,52 @@
+package Clase6;
+
+import Clase6.Ejemplo.ItemCompra;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    Calculadora calculadoraTest;
+
+    @BeforeEach
+    public void setUp() {
+        calculadoraTest = new Calculadora();
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSumar() {
+        double suma = calculadoraTest.sumar(150, 180);
+        double dividirSuma = calculadoraTest.dividir(suma, 3);
+        double resultadoEsperado = 110;
+        assertEquals(dividirSuma, resultadoEsperado);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void restar() {
+        double restar = calculadoraTest.restar(90, 50);
+        double restaMultiplicada = restar * 15;
+        double resultadoEsperado = 605;
+
+        boolean resultado = (restaMultiplicada != 605);
+        assertTrue(resultado);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void multiplicar() {
+        double multiplicacion = calculadoraTest.multiplicar(80, 3);
+        double resultadoEsperado = 240;
+
+        assertEquals(multiplicacion, resultadoEsperado);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSumar2() {
+        double suma2 = calculadoraTest.sumar(70, 40);
+        double multiplicarSuma = calculadoraTest.multiplicar(suma2, 25);
+
+        boolean resultado = (multiplicarSuma != 2700);
+        assertTrue(resultado);
+    }
+}
