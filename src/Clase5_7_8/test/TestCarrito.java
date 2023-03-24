9b32928b8a8502575244358d12be78a18c74a835
gfCrova <giancrova.cl5@gmail.com>
2023-03-24 00:12:33 -0300

    generate test of descuentos and carrito class

--- src/Clase5_7_8/test/TestCarrito.java
+++ src/Clase5_7_8/test/TestCarrito.java
@@ -1,2 +1,43 @@
-package Clase5_7_8.test;public class TestCarrito {
+package Clase5_7_8.test;
+
+import Clase5_7_8.Carrito;
+import Clase5_7_8.ItemCarrito;
+import Clase5_7_8.Producto;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCarrito {
+    Carrito carrito;
+
+    TestDescuentos testDescuentos;
+    ItemCarrito[] items = new ItemCarrito[3];
+
+    public void crearCarrito(){
+        carrito = new Carrito(items);
+
+        Producto producto1 = new Producto("Product1", 1500);
+        Producto producto2 = new Producto("Product2", 500);
+        Producto producto3 = new Producto("Product3", 1000);
+
+        ItemCarrito item1 = new ItemCarrito(producto1, 22);
+        ItemCarrito item2 = new ItemCarrito(producto2, 3);
+        ItemCarrito item3 = new ItemCarrito(producto3, 45);
+
+        items[0] = item1;
+        items[1] = item2;
+        items[2] = item3;
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testTotal() {
+
+        crearCarrito();
+
+        double resultadoTotal = carrito.calcularTotal();
+        double resultadoEsperado = 3000;
+
+        assertEquals(resultadoTotal, resultadoEsperado);
+    };
+    
 }
--- src/Clase5_7_8/test/TestDescuentos.java
+++ src/Clase5_7_8/test/TestDescuentos.java
@@ -1,2 +1,71 @@
-package Clase5_7_8.test;public class TestDescuentos {
+package Clase5_7_8.test;
+
+import Clase5_7_8.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDescuentos {
+
+    Carrito carrito;
+    ItemCarrito[] items = new ItemCarrito[3];
+    DescuentoFijo descuento;
+    DescuentoPorcentaje descuento2;
+    DescuentoPorcentajeConTope descuento3;
+
+    public void crearCarrito(){
+        carrito = new Carrito(items);
+
+        Producto producto1 = new Producto("Product1", 1500);
+        Producto producto2 = new Producto("Product2", 500);
+        Producto producto3 = new Producto("Product3", 1000);
+
+        ItemCarrito item1 = new ItemCarrito(producto1, 22);
+        ItemCarrito item2 = new ItemCarrito(producto2, 3);
+        ItemCarrito item3 = new ItemCarrito(producto3, 45);
+
+        items[0] = item1;
+        items[1] = item2;
+        items[2] = item3;
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDescuentoFijo() {
+        crearCarrito();
+
+        descuento = new DescuentoFijo(250);
+
+        double result = descuento.calcularDescuento(carrito);
+        double resultadoEsperado = 2750;
+
+        assertEquals(result, resultadoEsperado);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDescuentoPorcentaje() {
+        crearCarrito();
+
+        descuento2 = new DescuentoPorcentaje(25);
+
+        double result = descuento2.calcularDescuento(carrito);
+        double resultadoEsperado = 2250;
+
+        assertEquals(result, resultadoEsperado);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testDescuentoPorcentajeConTope() {
+        crearCarrito();
+
+        descuento3 = new DescuentoPorcentajeConTope(40, 35);
+
+        double result = descuento3.calcularDescuento(carrito);
+
+        double resultadoEsperado = 1950;
+
+        assertEquals(result, resultadoEsperado);
+    }
 }
+
+
+
+
