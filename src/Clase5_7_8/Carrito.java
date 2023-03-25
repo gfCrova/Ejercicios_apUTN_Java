56348b703f5f9d4e4fc813e1529ae936c720fec7
gfCrova <giancrova.cl5@gmail.com>
2023-03-25 00:24:32 -0300

    update main, excepciones

--- src/Clase5_7_8/Carrito.java
+++ src/Clase5_7_8/Carrito.java
@@ -1,29 +1,30 @@
 package Clase5_7_8;
 
+import java.util.ArrayList;
+
 public class Carrito {
 
-    private ItemCarrito[] item = new ItemCarrito[3];
-    private Descuento descuento;
+    private ArrayList<ItemCarrito> item = new ArrayList<>();
 
     public Carrito() {
     }
 
-    public Carrito(ItemCarrito[] item) {
+    public Carrito(ArrayList<ItemCarrito> item) {
         this.item = item;
     }
 
-    public ItemCarrito[] getItem() {
+    public ArrayList<ItemCarrito> getItem() {
         return item;
     }
 
-    public void setItem(ItemCarrito[] item) {
+    public void setItem(ArrayList<ItemCarrito> item) {
         this.item = item;
     }
 
     public double calcularTotal() {
         double total = 0;
-        for (ItemCarrito obj : item) {
-            total += obj.getProducto().getPrecio();
+        for (ItemCarrito items : item) {
+            total += items.getProducto().getPrecio();
         }
         return total;
     }
--- src/Clase5_7_8/Main.java
+++ src/Clase5_7_8/Main.java
@@ -6,37 +6,42 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Carrito carrito = new Carrito();
 
-        int i = 0;
-        String archivo = "src\\Clase5\\archivo.txt";
-        for (String linea : Files.readAllLines(Paths.get(archivo))) {
-            System.out.println(linea);
-            String nombre = linea.split("; ")[0];
-            int cantidad = Integer.parseInt(linea.split("; ")[1]);
-            double precio = Integer.parseInt(linea.split("; ")[2]);
+        try {
+            int i = 0;
+            String archivo = "src\\Clase5_7_8\\archivo.txt";
+            for (String linea : Files.readAllLines(Paths.get(archivo))) {
+                System.out.println(linea);
+                String nombre = linea.split("; ")[0];
+                int cantidad = Integer.parseInt(linea.split("; ")[1]);
+                double precio = Integer.parseInt(linea.split("; ")[2]);
 
-            Producto producto = new Producto(nombre, precio);
-            ItemCarrito itemsDeCarrito = new ItemCarrito(producto, cantidad);
+                Producto producto = new Producto(nombre, precio);
+                ItemCarrito itemsDeCarrito = new ItemCarrito(producto, cantidad);
 
-            carrito.getItem()[i] = itemsDeCarrito;
-            i++;
+                carrito.getItem().add(i, itemsDeCarrito);
+                i++;
+            }
+            System.out.println("Importe Total: $" + carrito.calcularTotal() + "\n");
+
+            DescuentoFijo descFijo = new DescuentoFijo(25);
+            System.out.println("Con Descuento Fijo de $ " + descFijo.getValorDescuento());
+            System.out.println("Importe Total es: $" + descFijo.calcularDescuento(carrito) + "\n");
+
+            DescuentoPorcentaje descPorcentaje = new DescuentoPorcentaje(15);
+            System.out.println("Con Descuento de " + descPorcentaje.getValorDescuento() + "%");
+            System.out.println("Importe Total es: $" + descPorcentaje.calcularDescuento(carrito) + "\n");
+
+            DescuentoPorcentajeConTope descPorcentajeConTope2 = new DescuentoPorcentajeConTope(50, 30);
+            // Si el descuento es mayor al valor del tope se anula y se descuenta el valor del tope!
+            System.out.println("Con Descuento de " + descPorcentajeConTope2.getValorDescuento() + "% y Tope Fijo de " + descPorcentajeConTope2.getValorTope() + "%");
+            System.out.println("Importe Total es: $" + descPorcentajeConTope2.calcularDescuento(carrito));
+
+        } catch (IOException e) {
+                System.out.println("No se encontro el archivo " + e.getMessage());
         }
-        System.out.println("Importe Total: $" + carrito.calcularTotal() + "\n");
-
-        DescuentoFijo descFijo = new DescuentoFijo(25);
-        System.out.println("Con Descuento Fijo de $ " + descFijo.getValorDescuento());
-        System.out.println("Importe Total es: $" + descFijo.calcularDescuento(carrito) + "\n");
-
-        DescuentoPorcentaje descPorcentaje = new DescuentoPorcentaje(15);
-        System.out.println("Con Descuento de " + descPorcentaje.getValorDescuento() + "%");
-        System.out.println("Importe Total es: $" + descPorcentaje.calcularDescuento(carrito) + "\n");
-
-        // Si el descuento es mayor al valor del tope se anula y se descuenta el valor del tope!
-        DescuentoPorcentajeConTope descPorcentajeConTope2 = new DescuentoPorcentajeConTope(50, 30);
-        System.out.println("Con Descuento de " + descPorcentajeConTope2.getValorDescuento()  + "% y Tope Fijo de " + descPorcentajeConTope2.getValorTope() + "%");
-        System.out.println("Importe Total es: $" + descPorcentajeConTope2.calcularDescuento(carrito));
     }
 }
