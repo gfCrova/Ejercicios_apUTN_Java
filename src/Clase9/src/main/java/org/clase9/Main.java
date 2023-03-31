91077c5ee182ef5009f644daeafa676f248bb5ea
gfCrova <giancrova.cl5@gmail.com>
2023-03-31 18:27:20 -0300

    agregando todas las clases

---
+++ src/Clase9/src/main/java/org/clase9/Main.java
@@ -0,0 +1,12 @@
+package org.clase9;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PersonaService services = new PersonaService();
+        services.ingresarPersonas();
+
+    }
+
+}
