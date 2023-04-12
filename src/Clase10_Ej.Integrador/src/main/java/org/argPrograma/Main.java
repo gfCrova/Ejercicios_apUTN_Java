bb219074847afbae72da21de864643c0199a76c3
gfCrova <giancrova.cl5@gmail.com>
2023-04-12 00:29:19 -0300

    update main method

--- src/Clase10_Ej.Integrador/src/main/java/org/argPrograma/Main.java
+++ src/Clase10_Ej.Integrador/src/main/java/org/argPrograma/Main.java
@@ -6,19 +6,12 @@ import org.argPrograma.services.FilesService;
 import java.io.IOException;
 
 public class Main {
-    private static final FilesService fileServicio = new FilesService();
-    private static final CorrelativasService correlativeSrv = new CorrelativasService(fileServicio);
-    public static void main(String[] args) {
 
-        // Nueva inscripción
-        // leer archivo de inscripciones
-        try {
-            correlativeSrv.registrarMaterias();
-            correlativeSrv.registrarAlumnos();
+    public static void main(String[] args) throws IOException {
 
-            correlativeSrv.registrarInscripcion();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        FilesService fileServicio = new FilesService();
+        CorrelativasService correlativasService = new CorrelativasService(fileServicio);
+
+        correlativasService.registrarInscripcion();
     }
 }
