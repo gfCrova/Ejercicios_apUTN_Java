7aedce782478c63e989af9687bc9215f1662c499
gfCrova <giancrova.cl5@gmail.com>
2023-04-13 23:10:53 -0300

    sucesivos cambios en estructura y funcionamiento de las funciones

--- src/Clase10_Ej.Integrador/src/main/java/org/argPrograma/services/CorrelativasService.java
+++ src/Clase10_Ej.Integrador/src/main/java/org/argPrograma/services/CorrelativasService.java
@@ -1,17 +1,19 @@
 package org.argPrograma.services;
 
-import org.argPrograma.entities.Inscripcion;
+import org.argPrograma.entities.Alumno;
 import org.argPrograma.entities.InscripcionEnum;
 import org.argPrograma.entities.Materia;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CorrelativasService {
 
     private final FilesService fileServicio;
     private final AlumnosService alumnosService = new AlumnosService();
-    private List<String> materiasAprobadas;
+    private List<Materia> materiasAprobadas;
     private List<String> materiasCorrelativas;
 
     public CorrelativasService(FilesService fileServicio) {
@@ -19,7 +21,7 @@ public class CorrelativasService {
     }
 
     public void buscarCorrelativas(String materia) throws IOException{
-        String lineaMateria = fileServicio.buscarCorrelativa(materia);
+        String lineaMateria = buscarMateria(materia);
         String[] arrCorrelativas = lineaMateria.split(",");
 
         materiasCorrelativas = new ArrayList<String>();
@@ -28,38 +30,49 @@ public class CorrelativasService {
             for (int i = 1; i < arrCorrelativas.length; i++) {
                 materiasCorrelativas.add(arrCorrelativas[i]);
             }
-            System.out.println("Correlativas para la materia '" + materia + "' son: ");
-            System.out.println(materiasCorrelativas);
+            System.out.println("Correlativas para la materia '" + materia + "' son: " + Arrays.toString(materiasCorrelativas.get(0).split(" ")));
         }
     }
 
-    public void buscarMateriasAprobadas(int legajo) throws IOException {
+    public List<Materia> buscarMateriasAprobadas(int legajo) throws IOException {
         String lineaAlumno = alumnosService.buscarAlumnos(legajo);
-        String[] arrAlumno = lineaAlumno.split(",");
+        List<String> arrAlumno = List.of(lineaAlumno.split(","));
 
-        materiasAprobadas = new ArrayList<String>();
+        materiasAprobadas = new ArrayList<Materia>();
 
-        if (arrAlumno.length > 2) {
-            for (int i = 2; i < arrAlumno.length; i++) {
-                materiasAprobadas.add(arrAlumno[i]);
+        if (arrAlumno.size() > 2) {
+            for (int i = 2; i < arrAlumno.size(); i++) {
+                Materia mt = new Materia(arrAlumno.get(i));
+                materiasAprobadas.add(mt);
             }
-            System.out.println("Materias Rendidas: " + materiasAprobadas);
+            System.out.println("Materias Aprobadas: " + Arrays.toString(materiasAprobadas.get(0).getNombre().split(" ")));
         }
+        return materiasAprobadas;
     }
 
     public InscripcionEnum validarInscripcion() {
 
-        InscripcionEnum aprobada = InscripcionEnum.APROBADA;
-        InscripcionEnum rechazada = InscripcionEnum.RECHAZADA;
-
-        for (String correlativa : materiasCorrelativas ) {
-            if (materiasAprobadas.contains(correlativa)) {
-                System.out.println("SOLICITUD: " + aprobada);
-                return aprobada;
+        for (int i = 0; i < materiasCorrelativas.size(); i++) {
+            if (materiasCorrelativas.contains(materiasAprobadas.get(i).getNombre())) {
+                System.out.println("SOLICITUD: " + InscripcionEnum.APROBADA);
+                return InscripcionEnum.APROBADA;
             }
-            break;
         }
-        System.out.println("SOLICITUD: " + rechazada);
-        return rechazada;
+        System.out.println("SOLICITUD: " + InscripcionEnum.RECHAZADA);
+        return InscripcionEnum.RECHAZADA;
+    }
+
+    public String buscarMateria(String materia) throws IOException {
+
+        List<String> readMaterias = fileServicio.leerMaterias();
+        String lineaMateria = "";
+
+        for (String linea : readMaterias) {
+            String mat = linea.split(",")[0];
+            if (materia.equals(mat)) {
+                lineaMateria = linea;
+            }
+        }
+        return lineaMateria;
     }
 }
