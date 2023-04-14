8e9cc0355137a797d2e863ca00e811fadd42ad08
gfCrova <giancrova.cl5@gmail.com>
2023-04-13 23:08:49 -0300

    mejorando los metodos de buscar y registrar alumnos

--- src/Clase10_Ej.Integrador/src/main/java/org/argPrograma/services/AlumnosService.java
+++ src/Clase10_Ej.Integrador/src/main/java/org/argPrograma/services/AlumnosService.java
@@ -14,30 +14,28 @@ public class AlumnosService {
     private final FilesService fileServicio = new FilesService();
 
     public String buscarAlumnos(int legajo) throws IOException {
-        String lineaAlumno = "";
+        String lineStr = "";
         List<String> lineaAlumnos = fileServicio.leerAlumnos();
 
         for (int i = 0; i < lineaAlumnos.size(); i++) {
             if (i != 0) {
                 String lineas = lineaAlumnos.get(i);
                 int leg = Integer.parseInt(lineas.split(",")[1]);
-                String materiasApr = lineas.split(",")[2];
-
                 if (leg == legajo) {
-                    lineaAlumno = lineas;
+                    lineStr = lineas;
                 }
             }
         }
-        return lineaAlumno;
+        return lineStr;
     }
 
-    public void registrarAlumnos(String nombre, int legajo, Materia materia, InscripcionEnum solicitud) throws IOException{
+    public void registrarAlumnos(String nombre, int legajo, Materia matDeInscripcion, List<Materia> matAprobadas, InscripcionEnum solicitud) throws IOException{
 
-        Alumno alumno = new Alumno(nombre, legajo);
+        Alumno alumno = new Alumno(nombre, legajo, matAprobadas);
         ArrayList<Alumno> alumnos = new ArrayList<>();
         alumnos.add(new Alumno(nombre, legajo));
 
-        Inscripcion inscripcion = new Inscripcion(alumno, materia, solicitud);
+        Inscripcion inscripcion = new Inscripcion(alumno, matDeInscripcion, solicitud);
 
         for (int i = 0; i < alumnos.size(); i++) {
             fileServicio.guardarResultados(inscripcion);
